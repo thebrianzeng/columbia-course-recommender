c897d8b3dab22cf7c08821216461eb62fc9c931e
thebrianzeng <thebrianzeng@gmail.com>
2015-02-06 23:00:29 -0500

    can calculate similarities

--- get_counts.py
+++ get_counts.py
@@ -1,8 +1,11 @@
+import logging
 import nltk
 from sqlalchemy.orm import sessionmaker, load_only
 from gensim import corpora, models, similarities
 
 from schema import engine, Professor, Review, Course, ShortenedCourseReview
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -30,7 +33,6 @@ def get_counts_for_class(class_id):
     try:
         reviews = get_reviews_for_class(class_id)
     except:
-        # print class_id
         return
     tokens = []
     for review in reviews:
@@ -55,27 +57,51 @@ def get_counts_for_class(class_id):
     # return [words]
 
 
-def create_dictionary(texts):
+def create_dictionary_and_corpus():
+    stoplist = set('for a of the and to in'.split())
+    # documents = session.query(Review).all()
+    documents = session.query(Review).filter(Review.course_id == 73).all()
+
+    texts = [[word for word in document.review.lower().split() if word not in
+              stoplist]
+             for document in documents]
+    all_tokens = sum(texts, [])
+    tokens_once = set(word for word in set(all_tokens) if
+                      all_tokens.count(word) == 1)
+    texts = [[word for word in text if word not in tokens_once]
+             for text in texts]
     dictionary = corpora.Dictionary(texts)
     corpus = [dictionary.doc2bow(text) for text in texts]
-    # corpus = dictionary.doc2bow(words[0])
-    # print corpus
-    # print dictionary
     dictionary.save('/tmp/reviews.dict')
     corpora.MmCorpus.serialize('/tmp/corpus.mm', corpus)
-    return dictionary, corpus
+    tfidf = models.TfidfModel(corpus)
+    corpus_tfidf = tfidf[corpus]
+    lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=100)
+    corpus_lsi = lsi[corpus_tfidf]
+    lsi.print_topics(100)
+    lsi.save('/tmp/model.lsi')
+    index = similarities.MatrixSimilarity(lsi[corpus])
+    doc = session.query(Review).filter(Review.course_id == 73).first().review
+    print doc
+    vec_bow = dictionary.doc2bow(doc.lower().split())
+    vec_lsi = lsi[vec_bow]
+    sims = index[vec_lsi]
+    print(list(enumerate(sims)))
+    # return dictionary, corpus
 
 
 def generate_shortened_course_reviews():
-    course_ids = {course.id
-                  for course in
-                  session.query(Course).options(load_only("id")).all()}
-    for course_id in course_ids:
-        get_counts_for_class(course_id)
+    # course_ids = {course.id
+    #               for course in
+    #               session.query(Course).options(load_only("id")).all()}
+    # for course_id in course_ids:
+    #     get_counts_for_class(course_id)
+    pass
 
 
 def main():
-    generate_shortened_course_reviews()
+    create_dictionary_and_corpus()
+    # generate_shortened_course_reviews()
     # get_counts_for_class(1908)
     # get_counts_for_class(73)
     # words = get_counts_for_class(73)
